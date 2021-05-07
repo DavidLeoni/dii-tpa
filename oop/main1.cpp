259d8b3ef7b439e42d9e6f669ec71842b7163dcd
David Leoni <noreply@davidleoni.it>
2021-05-07 09:48:39 +0200

    first version geometry solution, no tests

---
+++ oop/main1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdexcept>
+
+
+using namespace std;
+
+ class Point
+ {
+    private :
+        int x , y ;
+    public :
+        Point ( int u , int v ) : x ( u ) , y ( v ) {}
+        int getX () const { return x ; }
+        int getY () { return y ; }
+        void doubleVal ()
+        {
+            x *= 2;
+            y *= 2;
+        }
+};
+
+int main ()
+{
+    Point myPoint (5 , 3);
+    myPoint.doubleVal();
+    cout << myPoint.getX () << " " << myPoint.getY () << " \ n " ;
+    return 0;
+}
---
+++ oop/main2.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <stdexcept>
+
+
+using namespace std;
+
+ class Point
+ {
+ private :
+    int x , y ;
+ public :
+    Point ( int u , int v ) : x ( u ) , y ( v ) {}
+    int getX () { return x ; }
+    int getY () { return y ; }
+    };
+ 
+ int main ()
+ {
+    Point * p = new Point (5 , 3) ;
+
+    cout << p->getX () << ' ' << p->getY () ;
+    return 0;
+}
